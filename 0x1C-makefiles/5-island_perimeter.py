095f6cee8dd72afa5a785df12dc0f79a5397957b
NedzunganiK <Nedzunganik8@gmail.com>
2024-02-26 21:37:19 -0800

    C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,16 +14,17 @@ def island_perimeter(grid):
     """
     width = len(grid[0])
     height = len(grid)
-    edges = 0
-    size = 0
+    perimeter = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                perimeter += 4  # Each cell contributes 4 to the perimeter
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Reduce 2 if there's a neighboring land on the left
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Reduce 2 if there's a neighboring land on the top
+    return perimeter
 
